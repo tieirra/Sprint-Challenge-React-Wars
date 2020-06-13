7ca0762083739101594efe7dbbfa7aac972669c1
user <user@users-MacBook-Air.local>
2020-06-13 13:27:07 -0500

    fixed reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import axios  from 'axios';
+import People from "./components/Character";
+import {Row} from "reactstrap"
+import axios from 'axios';
 
-const App = () => {
+
+
+
+ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [characters, setCharacters ] = useState({});
-const [error , setError] = useState({});
-
-
-useEffect(() => {
-axios.get ('https://swapi.dev/')
-
-    .then ((response) => {console.log(response.data) 
-      setCharacters(response.data)
-    })
-
+  const [character, setCharacter] = useState({});
+ 
+  useEffect(() => {
     
-    .catch(error => {
-    console.log(error);
-    })
+    axios.get('https://swapi.dev/')
 
-}, []); 
+    .then(resp=> setCharacter(resp.data.results))
 
+      .catch(error => {
+        console.log(error);
+      });
+
+  }, []);
+
+console.log(character)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
 
 
@@ -32,6 +34,15 @@ axios.get ('https://swapi.dev/')
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      <Row>
+     { character.map((characterInfo=>{
+        return <People image={characterInfo.image} name={characterInfo.name} status={characterInfo.status} species={characterInfo.species} img={characterInfo.status} key={characterInfo.id} note={characterInfo.type}/> 
+
+      }))}
+
+      </Row>
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import {Card, Col, CardText, CardTitle, CardImg } from 'reactstrap'
+
+
+ const People = props => {
+    return(
+    <Card outline color='warning' style={{padding:'7%',margin:'7%',backgroundColor:'#red'}}>
+    <CardImg src={props.image} />
+    <CardTitle><b>Name:</b> <u>{props.name}</u></CardTitle>
+    <CardText><b>Species:</b> {props.species}</CardText>
+    <CardText><b>Gender:</b> {props.gender}</CardText>
+    </Card>
+    )
+};
+
+export default People;
