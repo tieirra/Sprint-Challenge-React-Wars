b489c3641580e4b08019354f89848f6b679de3b7
user <user@users-MacBook-Air.local>
2020-06-13 23:50:03 -0500

    adding final code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,50 +1,47 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import People from "./components/Character";
-import {Row} from "reactstrap"
+import People  from "./components/Character"; 
 import axios from 'axios';
 
 
 
 
- const App = () => {
+const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [character, setCharacter] = useState({});
- 
+  const [character, setCharacter] = useState([]);
+
   useEffect(() => {
-    
     axios.get('https://swapi.dev/')
 
-    .then(resp=> setCharacter(resp.data.results))
+      .then((resp) => {
+        setCharacter(resp.data.results);
+      })
 
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      });
+
+      })
 
   }, []);
 
-console.log(character)
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
 
 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-
-      <Row>
-     { character.map((characterInfo=>{
-        return <People image={characterInfo.image} name={characterInfo.name} status={characterInfo.status} species={characterInfo.species} img={characterInfo.status} key={characterInfo.id} note={characterInfo.type}/> 
-
-      }))}
-
-      </Row>
-      
+      <div className="cardHolder">
+        <People data={character} />
+      </div>
     </div>
   );
-}
+};
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,17 +1,71 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react'
-import {Card, Col, CardText, CardTitle, CardImg } from 'reactstrap'
+import React from 'react';
+import styled from 'styled-components';
 
 
- const People = props => {
-    return(
-    <Card outline color='warning' style={{padding:'7%',margin:'7%',backgroundColor:'#red'}}>
-    <CardImg src={props.image} />
-    <CardTitle><b>Name:</b> <u>{props.name}</u></CardTitle>
-    <CardText><b>Species:</b> {props.species}</CardText>
-    <CardText><b>Gender:</b> {props.gender}</CardText>
-    </Card>
-    )
+
+
+const People = (props) => {
+    const { data } = props;
+
+    return (
+        <CardHolderDiv>
+            {data.map((characterInfo) => {
+                return (
+
+                    <Card>
+                        <h1>Name: {characterInfo.name}</h1>
+                        <h3>Gender: {characterInfo.gender}</h3>
+                        <h3>Status: {characterInfo.status}</h3>
+                        <h3>Spieces: {characterInfo.species}</h3>
+                        <ImgPicture>Picture</ImgPicture>>
+                        <Img src={characterInfo.image}></Img>
+                    </Card>
+
+                );
+
+
+            })}
+
+        </CardHolderDiv>
+    );
+
+
 };
+
+const CardHolderDiv = styled.div`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  display: flex;
+  align-items: center;
+`;
+
+
+const Card = styled.div`
+  background-color: #fff;
+  border: solid 1px black;
+  border-radius: 2%;
+  width: 25%;
+  margin: 2% 0;
+  padding: 2%;
+  box-shadow: 5px 5px 20px red;
+  color: #555;
+`;
+
+
+const Img = styled.img`
+  border: gray solid 3px;
+  border-radius: 5%;
+  max-width: 100%;
+`;
+
+
+const ImgPicture = styled.p`
+  color: pink;
+  font-size: 15px;
+`;
+
+
 
 export default People;
